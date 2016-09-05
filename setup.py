2af6b31e4cdbbe6de55cc78b99577f8bc8ef0a0e
Rusty <revoltingrobot@gmail.com>
2016-09-05 18:48:04 -0400

    fixed setup

--- setup.py
+++ setup.py
@@ -5,4 +5,4 @@ setup(
     name = "pattern_watch",
     version = "0.1",
     description = "Monitor and move incoming files",
-    executables = [Executable("pattern_watch.py", base = "Win32GUI")])
+    executables = [Executable("pattern_watch.py")])
